718db2da3d904fba6e0f5c7cfbff87eed0017375
kasperbnielsen <32563463+kasperbnielsen@users.noreply.github.com>
2023-07-14 23:23:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [ 
-            {
-                protocol: 'https',
-                hostname: 'upload.wikimedia.org',
-                port: '',
-                pathname: 'wikipedia/commons/thumb/a/a7/React-icon.svg/**'
-            },
-        ],
-    },
-
+    output: "export";
 }
 
 module.exports = nextConfig
